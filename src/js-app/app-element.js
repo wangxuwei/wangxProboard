7fabb0d26f422155d35e91ec2b488e16b7bea4af
wangxuwei <wangxuwei84@gmail.com>
2017-01-25 16:32:47 +0800

    . add drag support to reorder for 2 level features without saving

--- src/js-app/app-element.js
+++ src/js-app/app-element.js
@@ -6,3 +6,19 @@ app.elFrom = function(str){
 	node.innerHTML = str;
 	return node.firstChild;
 };
+
+app.elCopy = function(el){
+	var html = el.outerHTML;
+	var copyEl = app.elFrom(html);
+	return copyEl;
+};
+
+app.elAbsOffset = function(el){
+	var offset = {left: el.offsetLeft, top: el.offsetTop};
+	if(el.offsetParent != null){
+		var topOffset = app.elAbsOffset(el.offsetParent);
+		offset.left += topOffset.left;
+		offset.top += topOffset.top;
+	}
+	return offset; 
+}
--- src/view/Dashboard.js
+++ src/view/Dashboard.js
@@ -26,11 +26,98 @@ d.register("Dashboard",{
 					inputEl.value = "";
 				});
 			}
+		},
+
+		"click; .table-header .row .cell.feature .edit": function(evt){
+			var view = this;
+			var targetEl = evt.target;
+			var tableEl = d.closest(targetEl, ".table");
+			if(targetEl.classList.contains("active")){
+				targetEl.classList.remove("active");
+				targetEl.innerHTML = "edit";
+				tableEl.classList.remove("drag-edit-mode");
+			}else{
+				targetEl.classList.add("active");
+				targetEl.innerHTML = "view";
+				tableEl.classList.add("drag-edit-mode");
+			}
+		},
+
+		"mousedown; .row .drag-col .icon": function(evt){
+			var view = this;
+			var targetEl = evt.target;
+			var rowEl = d.closest(targetEl, ".row");
+			var rowCloneEl = app.elCopy(rowEl);
+			var rowsConEl = d.closest(targetEl, ".rows-con");
+
+			view._dragItem = rowCloneEl;
+			view._dragHolder = rowEl;
+
+			view._dragHolder.classList.add("drag-holder");
+			view._dragItem.classList.add("drag-item");
+			rowsConEl.appendChild(view._dragItem);
+
+			view._dragItem.style.left = evt.pageX + "px";
+			view._dragItem.style.top = evt.pageY + "px";
+			view._dragItem.style.opacity = .5;
 		}
 	}, 
 
+	docEvents: {
+		"mousemove": function(evt){
+			var view = this;
+			if(view._dragItem){
+				view._dragItem.style.left = evt.pageX + "px";
+				view._dragItem.style.top = evt.pageY + "px";
+
+				var rowsConEl = d.closest(view._dragItem, ".rows-con");
+				var rows = d.all(rowsConEl, ".row:not(.drag-item):not(.drag-holder)");
+				for(var i = 0; i < rows.length; i++){
+					var row = rows[i];
+					var rowOffset = app.elAbsOffset(row);
+					if(evt.pageX > rowOffset.left && evt.pageY > rowOffset.top && evt.pageX < rowOffset.left + row.clientWidth && evt.pageY < rowOffset.top + row.clientHeight){
+						if(evt.pageY > rowOffset.top + row.clientHeight / 2){
+							rowsConEl.insertBefore(view._dragHolder, row);
+							rowsConEl.insertBefore(row, view._dragHolder);
+						}else{
+							rowsConEl.insertBefore(view._dragHolder, row);
+						}
+
+						var dargNameEl = d.first(view._dragItem, ".name");
+						var holderNameEl = d.first(view._dragHolder, ".name");
+						var parentId = row.getAttribute("data-parent-id");
+						if(parentId){
+							view._dragItem.classList.add("secondary");
+							view._dragHolder.classList.add("secondary");
+						}else{
+							if(evt.pageX - rowOffset.left > 24){
+								view._dragItem.classList.add("secondary");
+								view._dragHolder.classList.add("secondary");
+							}else{
+								view._dragItem.classList.remove("secondary");
+								view._dragHolder.classList.remove("secondary");
+							}
+						}
+						break;
+					}
+				}
+			}
+		},
+
+		"mouseup": function(evt){
+			var view = this;
+			if(view._dragItem){
+				d.remove(view._dragItem);
+				view._dragHolder.classList.remove("drag-holder");
+				view._dragItem = null;
+				view._dragHolder = null;
+				// TODO: save the order
+			}
+		}
+	},
+
 	hubEvents: {
-		"dsHub; Feature": function(data,info){				
+		"dsHub; Feature": function(data,info){
 			refreshLists.call(this);
 		}
 	}
